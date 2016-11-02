31292fc46f05a69eed0e1baeb9b0501d4f08bde4
Jesse Ren <jesseren@MacBook-Pro-109.attlocal.net>
2016-11-02 02:15:41 -0700

    Flow of corporate booking estab

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,60 @@
-var Acuity = require('acuityscheduling');
-var userId = null;
-var apiKey = null;
+// alert("Yo!");
 
-var acuity = Acuity.basic({
-  userId: userId,
-  apiKey: apiKey
-});
+$(document).ready(function () {
+  //your code here
 
-acuity.request('/appointments', function (err, res, appointments) {
-  if (err) return console.error(err);
-  console.log(appointments);
-});
 
+
+
+  // $('.box').click(function() {
+  //     $('.box').each( function() {
+  //         if ($(this).offset().left < 0) {
+  //             $(this).css("left", "150%");
+  //         }
+  //     });
+  //
+  //     $(this).animate({
+  //          left: '-50%'
+  //      }, 500);
+  //
+  //      if ($(this).next().size() > 0) {
+  //          $(this).next().animate({
+  //              left: '50%'
+  //          }, 500);
+  //      } else {
+  //          $(this).prevAll().last().animate({
+  //              left: '50%'
+  //          }, 500);
+  //      }
+  // });
+  //
 
 
 var alertWorld = function(){
   alert("Yo!");
 }
+
+  $('#form2').hide();
+  $('#form3').hide();
+
+
+$('#button1').click(function(){
+  console.log("first button clicked");
+  $('#form1').fadeOut(100);
+  $('#form2').fadeIn(1500);
+});
+
+$('#button2').click(function(){
+  console.log("second button clicked");
+  $('#form2').fadeOut(100);
+  $('#form3').fadeIn(1000);
+});
+
+$('#button3').click(function(){
+  console.log("second button clicked");
+  $('#form3').fadeOut(100);
+  $('#form1').fadeIn(1000);
+});
+
+
+});
