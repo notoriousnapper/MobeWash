17e3f972ec3e4a09f689d76a77c38757a1689a53
Jesse Ren <jesseren@MacBook-Pro-109.attlocal.net>
2016-11-02 08:07:18 -0700

    App Listen

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ app.use(bodyParser.json())
 //   console.log(appointments);
 // });
 
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
 
 
 
